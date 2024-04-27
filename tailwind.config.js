7c978847ee63283b170d67c045352a1c9a87d0ec
JhoanSLC <JhoanSLC4@gmail.com>
2024-04-27 10:31:01 -0500

    feat: :sparkles: Inicio página tesla con astro

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro, html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Gotham SSm A, sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
